1dad97e5a6b35bf8ac7f8de0a829018261eeec29
kulkarni-harsh <kulkarni-harsh@github.com>
2021-10-23 20:52:36 +0530

    models created

---
+++ lib/models/Speaker.dart
@@ -0,0 +1,11 @@
+class Speaker {
+  final String name;
+  final String info;
+  final String imageUrl;
+  final String designation;
+  Speaker(
+      {required this.designation,
+      required this.name,
+      required this.info,
+      required this.imageUrl});
+}
---
+++ lib/models/User.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/rendering.dart';
+
+class User {
+  final String name;
+  final String email;
+  final int phoneNo;
+  final String? profilePicLink;
+
+  User(
+      {required this.name,
+      required this.email,
+      required this.phoneNo,
+      this.profilePicLink});
+}
