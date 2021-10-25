88f97669b1cf79b020ad718a78a2253720074429
kulkarni-harsh <kulkarni-harsh@github.com>
2021-10-25 21:18:34 +0530

    hompage colored layout

--- lib/models/User.dart
+++ lib/models/User.dart
@@ -12,3 +12,8 @@ class User {
       required this.phoneNo,
       this.profilePicLink});
 }
+
+User kUser = User(
+    name: "Harsh Kulkarni",
+    email: "harsh.kulkarni.42774@gmail.com",
+    phoneNo: 8888888888);
